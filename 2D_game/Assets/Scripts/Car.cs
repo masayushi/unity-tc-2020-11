e41ac04e65f690b14cea5e3242b23366be7bb442
masayushi <65478781+masayushi@users.noreply.github.com>
2020-12-03 21:34:58 +0800

    設定邊框、C#基礎介紹

---
+++ 2D_game/Assets/Scripts/Car.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //: 單行註解
+    // 數值 = 欄位(Field) 
+    // 語法如下:
+    // 類別 名稱 ; 
+    
+
+    //Unity 常見的4大類型如下:
+
+    //整數(int) : 沒有小數點的數值。ex:1、0、-99等
+    //浮點數(float) : 有小數點的數值。ex:1.5、3.14等
+    //字串(string) : 文字(包含中、英、日文)、特殊字型。ex:BMW、賓士、@#$等
+    //布林值(bool) : 有(true)、沒有(false)。用來下指令使用
+
+    //對應如下:
+
+    int size;
+    float weight;
+    string brand;
+    bool haswindow;
+
+}
